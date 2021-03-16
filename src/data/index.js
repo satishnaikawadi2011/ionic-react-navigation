b13a460af62d676eadddc5f9456b6cba2b35b613
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-17 00:58:23 +0530

    Added dummy data.

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+export const COURSES = [
+	{ id: 'c1', title: 'React - The Practical Guide' },
+	{ id: 'c2', title: 'Node JS - The Practical Guide' },
+	{ id: 'c3', title: 'Python - The Practical Guide' }
+];
--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,6 +1,19 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonHeader,
+	IonPage,
+	IonRow,
+	IonTitle,
+	IonToolbar
+} from '@ionic/react';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { COURSES } from '../data';
 
 const Courses: React.FC = () => {
 	const history = useHistory();
@@ -16,10 +29,26 @@ const Courses: React.FC = () => {
 			</IonHeader>
 			<IonContent>
 				<h2>Welcome to courses page !!!</h2>
-				<div>
-					{/* <IonButton routerLink="/course-sections">To Course Sections Page</IonButton> */}
+				{/* <div>
+					<IonButton routerLink="/course-sections">To Course Sections Page</IonButton>
 					<IonButton onClick={changePageHandler}>To Course Sections Page</IonButton>
-				</div>
+				</div> */}
+				<IonGrid>
+					{COURSES.map((course) => {
+						return (
+							<IonRow key={course.id}>
+								<IonCol sizeMd="4" offsetMd="4">
+									<IonCard>
+										<IonCardContent className="ion-text-center">
+											<h2>{course.title}</h2>
+											<IonButton>view Course Sections</IonButton>
+										</IonCardContent>
+									</IonCard>
+								</IonCol>
+							</IonRow>
+						);
+					})}
+				</IonGrid>
 			</IonContent>
 		</IonPage>
 	);
