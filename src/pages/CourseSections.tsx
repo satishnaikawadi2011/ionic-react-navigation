35445bbfe89bdac987af9d78ad86a35f01d9e339
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-17 00:06:54 +0530

    Added IonBackButton in IonToolbar to pop the current page.

--- src/pages/CourseSections.tsx
+++ src/pages/CourseSections.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const CourseSections: React.FC = () => {
@@ -6,6 +6,9 @@ const CourseSections: React.FC = () => {
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
+					<IonButtons slot="start">
+						<IonBackButton defaultHref="/" />
+					</IonButtons>
 					<IonTitle>Course Sections</IonTitle>
 				</IonToolbar>
 			</IonHeader>
