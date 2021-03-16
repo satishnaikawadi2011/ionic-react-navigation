2377f33e0997bd622b20c5803e790b2b519c6b0c
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-17 00:10:58 +0530

    Added programatic navigation with the help of history.

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,7 +1,12 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Courses: React.FC = () => {
+	const history = useHistory();
+	const changePageHandler = () => {
+		history.push('/course-sections');
+	};
 	return (
 		<IonPage>
 			<IonHeader>
@@ -12,7 +17,8 @@ const Courses: React.FC = () => {
 			<IonContent>
 				<h2>Welcome to courses page !!!</h2>
 				<div>
-					<IonButton routerLink="/course-sections">To Course Sections Page</IonButton>
+					{/* <IonButton routerLink="/course-sections">To Course Sections Page</IonButton> */}
+					<IonButton onClick={changePageHandler}>To Course Sections Page</IonButton>
 				</div>
 			</IonContent>
 		</IonPage>
